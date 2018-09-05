d4f3800ee6996091ae14a340e58b53eebabe1de0
DengHaiYang1234 <429078038@qq.com>
2018-09-05 16:35:11 +0800

    2018.9.5
    双向链表。90%

---
+++ Day_DataStructure/007.双链表_4/DoubleLinkList.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+	public class DoubleLinkList<T>
+	{
+		Node<T> head;
+		int size;
+
+		public DoubleLinkList()
+		{
+			head = new Node<T>(default(T), null, null);
+			head.Prev = head;
+			head.Next = head;
+		}
+
+
+		Node<T> GetNode(int index)
+		{
+			if(index < 0 || index >= size)
+			{
+				throw new Exception("索引超出界限");
+			}
+
+			if(index < size /2)
+			{
+				Node<T> temp = head.Next;
+				for(int i = 0;i < index;i++)
+				{
+					temp = temp.Next;
+				}
+				return temp;
+			}
+
+			Node<T> _temp = head.Prev;
+			int rIndex = size - index - 1;
+			for(int i = 0;i < rIndex;i++)
+			{
+				_temp = _temp.Prev;
+			}
+
+			return _temp;
+		}
+
+		public T GetFirst() => GetNode(0).Data;
+		public T GetLast() => GetNode(size - 1).Data;
+		public T Get(int index) => GetNode(index).Data;
+
+		public void Append(int index,T value)
+		{
+			Node<T> iNode;
+			if(index == 0)
+			{
+				iNode = head;
+			}
+			else
+			{
+				index = index - 1;
+				if (index < 0)
+					throw new IndexOutOfRangeException("数组越界");
+				iNode = GetNode(index);
+			}
+
+			Node<T> tNode = new Node<T>(value, iNode, iNode.Next);
+			iNode.Next.Prev = tNode;
+			iNode.Next = tNode;
+			size++;
+		}
+
+		public void Insert(int index,T value)
+		{
+			if(index == 0)
+			{
+				Append(index, value);
+			}
+			else
+			{
+				Node<T> iNode = GetNode(index);
+				Node<T> tNode = new Node<T>(value,iNode.Prev,iNode);
+				iNode.Prev.Next = tNode;
+				iNode.Prev = tNode;
+				size++;
+			}
+		}
+
+		public void Del(int index)
+		{
+			Node<T> temp = GetNode(index);
+			temp.Prev.Next = temp.Next;
+			temp.Next.Prev = temp.Prev;
+		}
+
+		public void DelFirst() => Del(0);
+		public void DelLast() => Del(size - 1);
+
+		public void ShowAll()
+		{
+			Console.WriteLine("---------------------Start----------------");
+			for(int i = 0;i< size;i++)
+			{
+				Console.WriteLine("<" + i + ">:" + Get(i));
+			}
+			Console.WriteLine("---------------------End----------------");
+		}
+	}
+}
---
+++ Day_DataStructure/007.双链表_4/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+	public class Node<T>
+	{
+		public T Data { get; set; }
+		public Node<T> Next { get; set; }
+		public Node<T> Prev { get; set; }
+
+		public Node(T value,Node<T> prev,Node<T> next)
+		{
+			Data = value;
+			Prev = prev;
+			Next = next;
+		}
+	}
+}
