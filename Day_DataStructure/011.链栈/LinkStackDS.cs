24acb85b66fbebdec5706ba0bc12329913e83cd7
DengHaiYang1234 <429078038@qq.com>
2018-09-06 16:40:51 +0800

    2018.9.6
    链栈。注意理解先进后出的概念.先进后出.先进后出.先进后出.先进后出.先进后出。不懂画图

---
+++ Day_DataStructure/011.链栈/LinkStackDS.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+	public class LinkStackDS<T> : ILinkStack<T>
+	{
+		Node<T> head;
+		int top;
+
+		public LinkStackDS()
+		{
+			head = null;
+			top = 0;
+		}
+
+
+		public int Count
+		{
+			get
+			{
+				return top;
+			}
+		}
+
+		public void Clear()
+		{
+			head = null;
+		}
+
+		public bool Contains(T item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public T Peek()
+		{
+			return head.Data;
+		}
+
+		public T Pop()
+		{
+			T value = head.Data;
+			Node<T> temp = head.Next;
+			head = temp;
+			top--;
+			return value;
+		}
+
+		public void Push(T item)
+		{
+			Node<T> newNode = new Node<T>(item);
+			if(head == null)
+			{
+				head = newNode;
+				top++;
+				return;
+			}
+
+			newNode.Next = head;
+			head = newNode;
+			
+
+			top++;
+		}
+	}
+}
---
+++ Day_DataStructure/011.链栈/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+	public class Node<T>
+	{
+		public T Data { get; set; }
+		public Node<T> Next { get; set; }
+
+		public Node(T t)
+		{
+			Data = t;
+			Next = null;
+		}
+	}
+}
