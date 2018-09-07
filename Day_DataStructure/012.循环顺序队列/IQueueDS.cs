d612406ce1884429c9bf80c217351a44df59ba5c
DengHaiYang1234 <429078038@qq.com>
2018-09-07 09:56:58 +0800

    2018.9.7
    循环顺序队列

---
+++ Day_DataStructure/012.循环顺序队列/IQueueDS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+	public interface IQueueDS<T>
+	{
+		int Count { get; }
+		void Clear();
+		bool Contians(T item);
+		void Enqueue(T item);
+		T Dequeue();
+		T Peek();
+		bool IsEmpty();
+	}
+}
---
+++ Day_DataStructure/012.循环顺序队列/SeqQueue.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+	public class SeqQueue<T> : IQueueDS<T>
+	{
+		T[] arr;
+		int headIndex;
+		int endIndex;
+		int count;
+		public SeqQueue(int size)
+		{
+			arr = new T[size];
+			headIndex = -1;
+			endIndex = -1;
+			count = 0;
+		}
+
+
+		public SeqQueue() : this(3)
+		{
+
+		}
+
+
+		public int Count
+		{
+			get
+			{
+				return count;
+			}
+		}
+
+		public void Clear() => count = 0;
+		
+		public bool Contians(T item)
+		{
+			bool isHave = false;
+			for(int i = 0;i < arr.Length;i++)
+			{
+				if (arr[i].Equals(item))
+					isHave = true;
+			}
+
+			return isHave;
+		}
+
+		public T Dequeue()
+		{
+			if(headIndex == count - 1)
+			{
+				headIndex = -1;
+			}
+
+			T value = arr[headIndex + 1];
+			headIndex++;
+			count--;
+			return value;
+		}
+
+		public void Enqueue(T item)
+		{
+			if(endIndex == arr.Length - 1)
+			{
+				endIndex = -1;
+			}
+
+			arr[endIndex + 1] = item;
+			endIndex++;
+			if(count < arr.Length)
+			{
+				count++;
+			}
+			
+		}
+
+		public T Peek()
+		{
+			return arr[headIndex + 1];
+		}
+
+		public bool IsEmpty() => count == 0;
+		
+	}
+}
