e213bd11cea6b9437be50b84fc0863c9a9ffbbb0
DengHaiYang1234 <429078038@qq.com>
2018-09-15 14:32:07 +0800

    2018.9.15
    快速排序和随便写的排序算法

---
+++ Day_DataStructure/015.快速排序/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = { 1, 8, 5,2,5,6,9,8,7,1,2,5,86,5};
+			Sort(0, arr.Length - 1, arr);
+
+
+			for(int i = 0;i < arr.Length;i++)
+			{
+				Console.WriteLine(arr[i]);
+			}
+
+		}
+
+
+		static void Sort(int leftIndex, int rightIndex, int[] arr)
+		{
+			if(leftIndex >= rightIndex)
+			{
+				return; 
+			}
+
+			int midIndex = SortAndGetMidIndex(leftIndex, rightIndex, arr);
+
+			Sort(leftIndex, midIndex - 1, arr);
+
+			Sort(midIndex + 1, rightIndex, arr);
+		}
+
+
+		static int SortAndGetMidIndex(int leftIndex,int rightIndex,int[] arr)
+		{
+			int baseValue = arr[leftIndex];
+
+			while(leftIndex < rightIndex)
+			{
+
+				//从右至左  找小
+				while (leftIndex < rightIndex && baseValue < arr[rightIndex])
+					rightIndex--;
+
+				arr[leftIndex] = arr[rightIndex];
+				//从左至右  找大
+				while (leftIndex < rightIndex && baseValue >= arr[leftIndex])
+					leftIndex++;
+				arr[rightIndex] = arr[leftIndex]; 
+			}
+
+			arr[leftIndex] = baseValue;
+			return leftIndex;
+		}
+	}
+}
---
+++ Day_DataStructure/016.随便写的普通排序/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = { 1, 8,5,2,5,6,9,8,7,1,2,5,86,5};
+
+			Sort(arr);
+
+			for(int i = 0; i< arr.Length;i++)
+			{
+				Console.WriteLine(arr[i]);
+			}
+		}
+
+		static void Sort(int[] arr)
+		{
+			for(int i = 0; i < arr.Length;i++)
+			{
+				for(int j = i; j < arr.Length;j++)
+				{
+					if(arr[i] > arr[j])
+					{
+						var temp = arr[i];
+						arr[i] = arr[j];
+						arr[j] = temp;
+					}
+				}
+			}
+		}
+
+	}
+}
