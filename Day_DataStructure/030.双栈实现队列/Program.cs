a6b09d2bf41504a641ff6d6d4ad6e912f22f6bbe
DengHaiYang1234 <429078038@qq.com>
2018-09-28 20:57:48 +0800

    2018.9.28
    双队列实现栈及双栈实现队列

---
+++ Day_DataStructure/030.双栈实现队列/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> s1 = new Stack<int>();
+            Stack<int> s2 = new Stack<int>();
+            //入队列
+            EnQueue(s1, s2, 1);
+            EnQueue(s1, s2, 2);
+            EnQueue(s1, s2, 3);
+            EnQueue(s1, s2, 4);
+            EnQueue(s1, s2, 5);
+            //出队列
+            DeQueue(s1, s2);
+            DeQueue(s1, s2);
+            DeQueue(s1, s2);
+            DeQueue(s1, s2);
+            DeQueue(s1, s2);
+        }
+
+        static void EnQueue(Stack<int> s1,Stack<int> s2,int m)
+        {
+            s1.Push(m);
+            Console.WriteLine("入栈：" + m);
+        }
+
+        static void DeQueue(Stack<int> s1,Stack<int> s2)
+        {
+            if(s2.Count == 0)
+            {
+                int p = s1.Count;
+                for(int i = 0;i < p ;i++)
+                {
+                    s2.Push(s1.Peek());
+                    s1.Pop();
+                }
+            }
+            int m = s2.Peek();
+            s2.Pop();
+            Console.WriteLine("出栈：" + m);
+        }
+    }
+}
---
+++ Day_DataStructure/031.双队列实现栈/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q1 = new Queue<int>();
+            Queue<int> q2 = new Queue<int>();
+            StackPush(q1, q2, 1);
+            StackPush(q1, q2, 2);
+            StackPush(q1, q2, 3);
+            StackPush(q1, q2, 4);
+            StackPush(q1, q2, 5);
+            Console.WriteLine("------------------------------------------");
+            StaackPop(q1, q2);
+            StaackPop(q1, q2);
+            StaackPop(q1, q2);
+            StaackPop(q1, q2);
+            StaackPop(q1, q2);
+        }
+
+
+        static void StackPush(Queue<int> q1,Queue<int> q2,int m)
+        {
+            q1.Enqueue(m);
+            Console.WriteLine("入栈：" + m);
+        }
+
+
+        static void StaackPop(Queue<int> q1,Queue<int> q2)
+        {
+            int p = q1.Count;
+            for(int i =0;i< p - 1;i++)
+            {
+                q2.Enqueue(q1.Peek());
+                q1.Dequeue();
+            }
+            int m = q1.Peek();
+            q1.Dequeue();
+            Console.WriteLine("出栈：" + m);
+            int l = q2.Count;
+            for(int j = 0;j < l;j++)
+            {
+                q1.Enqueue(q2.Peek());
+                q2.Dequeue();
+            }
+        }
+
+       
+    }
+}
