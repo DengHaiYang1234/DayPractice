9f890892d6514d0829e92d37eed75e333ca84768
DengHaiYang1234 <429078038@qq.com>
2018-09-29 10:09:51 +0800

    2018.9.29
    顺序队列与链队列

---
+++ Day_DataStructure/032.顺序队列/SeqQueue.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class SeqQueue<T> : ISeqQueue<T>
+    {
+        T[] data;
+        int prev;
+        int end;
+        int count;
+
+        public SeqQueue(int size)
+        {
+            data = new T[size];
+            prev = -1;
+            end = -1;
+            count = 0;
+        }
+
+        public SeqQueue() : this(10)
+        {
+
+        }
+
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public void Clear()
+        {
+            prev = end = -1;
+            count = 0;
+        }
+        
+
+        public T Dequeue()
+        {
+            if(prev >= data.Length)
+            {
+                prev = -1;
+            }
+            T value = data[prev + 1];
+            prev++;
+            count--;
+            return value;
+        }
+
+        public void Enqueue(T item)
+        {
+            if(end >= data.Length)
+            {
+                end = -1;
+            }
+            data[end + 1] = item;
+            end++;
+            if(end < data.Length)
+            {
+                count++;
+            }
+        }
+
+        public T Peek()
+        {
+            return data[prev + 1];
+        }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Day_DataStructure/033.链队列/LinkQueue.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class LinkQueue<T> : ISeqQueue<T>
+    {
+        Node<T> prevNode;
+        Node<T> endNode;
+        int count;
+
+        public LinkQueue()
+        {
+            prevNode = null;
+            endNode = null;
+            count = 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public void Clear()
+        {
+            prevNode = null;
+            endNode = null;
+            count = 0;
+        }
+
+        public T Dequeue()
+        {
+            T value = prevNode.Data;
+            if (count == 1)
+            {
+                prevNode = null;
+                endNode = null;
+                count = 0;
+                return value;
+            }
+
+            Node<T> temp = prevNode.Next;
+            prevNode = temp;
+            count--;
+            return value;
+        }
+
+        public void Enqueue(T item)
+        {
+            Node<T> newItem = new Node<T>(item);
+            if(endNode == null)
+            {
+                endNode = newItem;
+                prevNode = newItem;
+                count++;
+                return;
+            }
+            endNode.Next = newItem;
+            endNode = newItem;
+            count++;
+        }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            return prevNode.Data;
+        }
+    }
+}
