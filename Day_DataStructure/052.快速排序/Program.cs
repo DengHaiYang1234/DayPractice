2ebd310f98f453d28aa4e394d4e90c9f2a8f9ad5
DengHaiYang1234 <429078038@qq.com>
2018-10-30 09:43:18 +0800

    2018.10.30
    排序算法

---
+++ Day_DataStructure/052.快速排序/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {3,1,1,8,9};
+
+            Sort(arr, 0, arr.Length - 1);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        static void Sort(int[] arr, int startIndex, int endIndex)
+        {
+            if (startIndex >= endIndex)
+                return;
+
+            int midIndex = SortAndGetMidIndex(arr, startIndex, endIndex);
+            Sort(arr, startIndex, midIndex - 1);
+            Sort(arr, midIndex + 1, endIndex);
+        }
+
+
+        static int SortAndGetMidIndex(int[] arr,int startIndex,int endIndex)
+        {
+            int baseValue = arr[startIndex];
+
+            while (startIndex < endIndex)
+            {
+                while (startIndex < endIndex && baseValue < arr[endIndex])
+                    endIndex--;
+
+                arr[startIndex] = arr[endIndex];
+
+                while (startIndex < endIndex && baseValue >= arr[startIndex])
+                    startIndex++;
+
+                arr[endIndex] = arr[startIndex];
+            }
+            arr[startIndex] = baseValue;
+            return startIndex;
+        }
+    }
+}
---
+++ Day_DataStructure/053.简单排序（我也不知道这是什么排序）/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {4, 8, 9, 3, 7, 1, 2, 38, 72};
+
+            Sort(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+
+        static void Sort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = arr.Length - 1; j > i; j--)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
