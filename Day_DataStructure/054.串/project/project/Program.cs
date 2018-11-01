8e5078973847a2290f1398cfad8f4af2dc72daa7
DengHaiYang1234 <429078038@qq.com>
2018-11-01 10:38:39 +0800

    2018.11.1
    串完成

--- Day_DataStructure/054.串/project/project/Program.cs
+++ Day_DataStructure/054.串/project/project/Program.cs
@@ -10,17 +10,33 @@ namespace project
     {
         static void Main(string[] args)
         {
-            StringDS a = new StringDS("12234");
+            StringDS a = new StringDS("12,23,4789");
             StringDS b = new StringDS("42");
 
             //Console.WriteLine("比较结果：" + StringDS.Compare(a, b));
 
-            string c = "12234";
+            string c = "12,23,4789";
             string d = "42";
 
-            
-            Console.WriteLine(c.IndexOf(d));
-            Console.WriteLine(a.IndexOf(b));
+
+            string[] ts = c.Split(',');
+
+
+            for(int i = 0; i< ts.Length;i++)
+                Console.WriteLine(ts[i]);
+
+
+            StringDS[] ds = a.Split(',');
+            Console.WriteLine("-----------------------------------------");
+            for (int i = 0; i < ds.Length; i++)
+                Console.WriteLine(ds[i]);
+
+
+            //Console.WriteLine(c.Split(','));
+
+
+
+            //Console.WriteLine(a.Substring(2,3));
         }
     }
 }
--- Day_DataStructure/054.串/project/project/StringDS.cs
+++ Day_DataStructure/054.串/project/project/StringDS.cs
@@ -142,5 +142,188 @@ namespace project
             }
             return baseIndex;
         }
+
+        public StringDS Remove(int stratIndex)
+        {
+            if (arr.Length == 0)
+            {
+                Console.WriteLine("Remove   数组为空");
+                return null;
+            }
+
+            char[] newChar = new char[stratIndex];
+
+            for (int i = 0; i < stratIndex; i++)
+            {
+                newChar[i] = arr[i];
+            }
+
+            arr = newChar;
+
+            return new StringDS(arr);
+        }
+
+        public StringDS Remove(int startIndex,int count)
+        {
+            int len = Count - count;
+
+            if (startIndex > len)
+            {
+                Console.WriteLine("越界了");
+                return null;
+            }
+
+            char[] newChar = new char[len];
+
+            int newIndex = 0;
+
+            for (int i = 0; i < startIndex; i++)
+            {
+                newChar[i] = arr[i];
+                newIndex = i + 1;
+            }
+
+
+            int index = startIndex + count;
+
+
+
+            for (int j = index; j < Count; j++)
+            {
+                newChar[newIndex + (j - index)] = arr[j];
+            }
+
+            arr = newChar;
+
+            return new StringDS(arr);
+        }
+
+        public StringDS Substring(int startIndex)
+        {
+            if (arr.Length == 0)
+            {
+                Console.WriteLine("为空");
+                return null;
+            }
+
+            int len = Count - startIndex;
+
+            char[] newChar = new char[len];
+
+            for (int i = startIndex; i < Count; i++)
+            {
+                newChar[i - startIndex] = arr[i];
+            }
+
+            arr = newChar;
+
+            return new StringDS(arr);
+        }
+
+        public StringDS Substring(int startIndex, int count)
+        {
+            int len = count;
+
+            char[] newChar = new char[len];
+
+            int maxIndex = startIndex + count;
+
+            for (int i = startIndex; i < maxIndex; i++)
+                newChar[i - startIndex] = arr[i];
+
+            arr = newChar;
+            return new StringDS(arr);
+        }
+
+        public StringDS[] Split(char sign)
+        {
+            if (arr.Length == 0)
+            {
+                Console.WriteLine("Split  数组为空");
+                return null;
+            }
+
+            int signNum = 0;
+
+            int[] signIndex = new int[signNum];
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (arr[i].Equals(sign))
+                {
+                    signNum += 1;
+
+                    if (signIndex.Length == 0)
+                    {
+                        int[] newArr = new int[signNum];
+                        newArr[newArr.Length - 1] = i;
+                        signIndex = newArr;
+                    }
+                    else
+                    {
+                        int[] newArr_ = new int[signNum];
+
+                        for (int j = 0; j < signIndex.Length; j++)
+                            newArr_[j] = signIndex[j];
+
+                        newArr_[newArr_.Length - 1] = i;
+                        signIndex = newArr_;
+                    }
+                }
+            }
+
+            if (signNum == 0)
+            {
+                Console.WriteLine("没有找到该符号");
+                return null;
+            }
+
+            StringDS[] newStrings = new StringDS[signNum + 1];
+
+            char[] newChar_1 = new char[signIndex[1]];
+            
+            int firstIndex = signIndex[1];
+            int endIndex = signIndex[signIndex.Length - 1] + 1;
+            char[] newChar_end = new char[Count - endIndex];
+
+            int index = 0;
+            for (int i = 0; i < signIndex[index]; i++)
+            {
+                newChar_1[i] = arr[i];
+            }
+
+            newStrings[index] = new StringDS(newChar_1);
+
+
+            for (int i = 1; i < signIndex.Length; i++)
+            {
+                int mid_index = signIndex[i];
+                int mid_pre_index = signIndex[i - 1];
+                int len = mid_index - mid_pre_index - 1;
+                char[] newChar_mid = new char[len];
+
+                int mid_start_index = mid_pre_index + 1;
+
+                for (int j = mid_start_index; j < mid_index; j++)
+                {
+                    newChar_mid[j - mid_start_index] = arr[j];
+                }
+                index += 1;
+
+                newStrings[index] = new StringDS(newChar_mid);
+
+            }
+
+
+            for (int i = endIndex; i < Count; i++)
+            {
+                newChar_end[i - endIndex] = arr[i];
+            }
+
+            newStrings[newStrings.Length - 1] = new StringDS(newChar_end);
+
+
+            return newStrings;
+        }
     }
 }
