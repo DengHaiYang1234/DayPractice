7fe142afb1a9dac3306c2bbb4b902edb4caf566d
DengHaiYang1234 <429078038@qq.com>
2018-11-14 09:47:51 +0800

    2018.11.14
    双栈实现队列
    双队列实现栈

---
+++ Day_DataStructure/067.双栈实现队列/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    //双栈实现队列
+    class Program
+    {
+        private static Stack<int> stack_1 = new Stack<int>();
+        private static Stack<int> stack_2 = new Stack<int>();
+
+        static void Main(string[] args)
+        {
+            Enqueue(1);
+            Enqueue(2);
+            Enqueue(3);
+            Enqueue(4);
+            Enqueue(5);
+            Console.WriteLine("===============出队列===============");
+            Console.WriteLine("======Dequeue:" + Dequeue());
+            Console.WriteLine("======Dequeue:" + Dequeue());
+            Console.WriteLine("======Dequeue:" + Dequeue());
+            Console.WriteLine("======Dequeue:" + Dequeue());
+            Enqueue(6);
+            Console.WriteLine("======Dequeue:" + Dequeue());
+        }
+
+
+        static void Enqueue(int value)
+        {
+            Console.WriteLine("入队列:" + value);
+            stack_1.Push(value);
+        }
+
+        static int Dequeue()
+        {
+            while (stack_1.Count > 0)
+            {
+                int value = stack_1.Pop();
+                stack_2.Push(value);
+            }
+
+            return stack_2.Pop();
+        }
+
+
+        
+    }
+}
---
+++ Day_DataStructure/068.双队列实现栈/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class Program
+    {
+        private static Queue<int> queue_1 = new Queue<int>();
+        private static Queue<int> queue_2 = new Queue<int>();
+
+        static void Main(string[] args)
+        {
+            Push(1);
+            Push(2);
+            Push(3);
+            Push(4);
+            Push(5);
+
+            Pop();
+            Pop();
+            Pop();
+            Pop();
+            Pop();
+            Pop();
+            Push(7);
+            Push(8);
+            Pop();
+        }
+
+        static void Push(int value)
+        {
+            Console.WriteLine("============入栈：" + value);
+            queue_1.Enqueue(value);
+        }
+
+        static int Pop()
+        {
+            int value = -1;
+            if (queue_1.Count > 0)
+            {
+                while (queue_1.Count > 1)
+                {
+                    int data = queue_1.Dequeue();
+                    queue_2.Enqueue(data);
+                }
+
+                value = queue_1.Dequeue();
+
+                while (queue_2.Count > 0)
+                {
+                    queue_1.Enqueue(queue_2.Dequeue());
+                }
+            }
+            else
+            {
+                Console.WriteLine("队列为空!!");
+            }
+            Console.WriteLine("============出栈：" + value);
+            return value;
+        }
+    }
+}
