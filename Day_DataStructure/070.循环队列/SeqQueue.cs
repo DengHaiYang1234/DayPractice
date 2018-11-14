aaa7fe7071a351f805f2c8be65796c30b817ec7c
DengHaiYang1234 <429078038@qq.com>
2018-11-14 14:24:08 +0800

    2018.11.14
    循环队列

---
+++ Day_DataStructure/070.循环队列/SeqQueue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class SeqQueue<T> : ISeqQueue<T>
+    {
+        private int front;
+        private int rear;
+        private int MAXSIZE;
+        private T[] arr;
+
+        public SeqQueue(int size)
+        {
+            arr = new T[size];
+            MAXSIZE = size;
+            front = rear = 0;
+        }
+
+        public SeqQueue() : this(5)
+        {
+            
+        }
+
+
+        public int Count
+        {
+            get { return (rear - front + MAXSIZE)%MAXSIZE; }
+        }
+
+        public void Clear()
+        {
+            front = rear = 0;
+        }
+
+        public T Dequeue()
+        {
+            if (front == rear)
+            {
+                Console.WriteLine("队列为空");
+                return default(T);
+            }
+
+            T value = arr[front];
+            front = (front + 1)%MAXSIZE;
+            return value;
+        }
+
+        public void Enqueue(T item)
+        {
+            if ((rear + 1)%MAXSIZE == front)
+            {
+                Console.WriteLine("队列 已满");
+                return;
+            }
+
+            arr[rear] = item;
+            rear = (rear + 1)%MAXSIZE;
+        }
+
+        public bool IsEmpty()
+        {
+            return (rear == 0) && (front == 0);
+        }
+
+        public T Peek()
+        {
+            return arr[front];
+        }
+    }
+}
