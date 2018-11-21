4ea0addf4836e7f7e1d4af594414cf8d4e9c789b
DengHaiYang1234 <429078038@qq.com>
2018-11-21 10:29:03 +0800

    2018.11.21
    双向循环链表

---
+++ Day_DataStructure/077.双向循环链表/DoubleLinkList.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class DoubleLinkList
+    {
+        private Node head;
+        private int count;
+
+        public DoubleLinkList()
+        {
+            head = null;
+            count = 0;
+        }
+
+
+        public Node GetElem(int index)
+        {
+            if (index < 0 || index > count)
+            {
+                Console.WriteLine("越界");
+                return null;
+            }
+
+            Node temp = head;
+
+            if (index < count/2)
+            {
+                for (int i = 0; i < index; i++)
+                    temp = temp.NextNode;
+            }
+            else
+            {
+                index = count - index;
+                for (int i = 0; i < index; i++)
+                    temp = temp.PreNode;
+            }
+
+            return temp;
+        }
+
+        public int GetFirst() => GetElem(0).Data;
+        public int GetLast() => GetElem(count - 1).Data;
+        public int Get(int index) => GetElem(index).Data;
+
+        public void Append(int index,int item)
+        {
+            if (index == 0 && head == null)
+            {
+                Node headItem = new Node(item, null, null);
+                head = headItem;
+                head.PreNode = head;
+                head.NextNode = head;
+            }
+            else
+            {
+                Node iNode = GetElem(index);
+                Node newItem = new Node(item,iNode,iNode.NextNode);
+                iNode.NextNode.PreNode = newItem;
+                iNode.NextNode = newItem;
+            }
+            count++;
+        }
+
+
+        public void Insert(int index,int item)
+        {
+            if (index == 0 && head == null)
+            {
+                Node headItem = new Node(item, null, null);
+                head = headItem;
+                head.PreNode = head;
+                head.NextNode = head;
+            }
+            else
+            {
+                Node iNode = GetElem(index);
+                Node newItem = new Node(item,iNode.PreNode,iNode);
+                iNode.PreNode.NextNode = newItem;
+                iNode.PreNode = newItem;
+                if (index == 0)
+                    head = newItem;
+            }
+            count++;
+        }
+
+        public void Delete(int index)
+        {
+            Node iNode = GetElem(index);
+            if (index == 0)
+            {
+                head = iNode.NextNode;
+                head.PreNode = iNode.PreNode;
+                iNode.PreNode.NextNode = head;
+            }
+            else
+            {
+                iNode.PreNode.NextNode = iNode.NextNode;
+                iNode.NextNode.PreNode = iNode.PreNode;
+            }
+
+            count--;
+        }
+
+        public void DelFirst() => Delete(0);
+        public void DelLast() => Delete(count - 1);
+        public void ShowAll()
+        {
+            Console.WriteLine("---------Start--------");
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("<" + i + ">:" + Get(i));
+            }
+            Console.WriteLine("---------End--------");
+        }
+
+    }
+}
---
+++ Day_DataStructure/077.双向循环链表/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class Node
+    {
+        public int Data { set; get; }
+        public Node PreNode { set; get; }
+        public Node NextNode { set; get; }
+
+        public Node(int item,Node pre,Node next)
+        {
+            Data = item;
+            PreNode = pre;
+            NextNode = next;
+        }
+    }
+}
