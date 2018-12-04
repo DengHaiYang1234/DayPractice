4286bc97ac47dd3d160eb0ff3b62b29f39a2c000
DengHaiYang1234 <429078038@qq.com>
2018-12-04 10:27:46 +0800

    2018.12.4
    顺序栈与链栈

---
+++ Day_DataStructure/093.顺序栈/SeqStack.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class SeqStack : ISeqStack
+    {
+        private int[] arr;
+        private int[] min;
+        private int top;
+
+        public SeqStack() : this(10)
+        {
+            
+        }
+
+        public SeqStack(int len)
+        {
+            arr = new int[len];
+            min = new int[len];
+            top = -1;
+        }
+
+        public int Count
+        {
+            get
+            {
+                int count = top;
+                return ++count;
+            }
+        }
+
+        public void Clear() => top = -1;
+        
+
+        public int Peek()
+        {
+            return arr[top];
+        }
+
+        public int Pop()
+        {
+            int value = arr[top];
+            top--;
+            return value;
+        }
+
+        public void Push(int item)
+        {
+            arr[top + 1] = item;
+            SetMin(item);
+            top++;
+        }
+
+
+        public void SetMin(int item)
+        {
+            if (top == -1)
+            {
+                min[top + 1] = item;
+            }
+            else
+            {
+                int topValue = min[top];
+                if (item < topValue)
+                    min[top + 1] = item;
+                else
+                    min[top + 1] = topValue;
+            }
+        }
+
+        public int GetMin()
+        {
+            return min[top];
+        }
+    }
+}
---
+++ Day_DataStructure/094.链栈/LinkStack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class LinkStack<T> : ILinkStack<T>
+    {
+        private Node<T> top;
+        private int count;
+
+        public LinkStack()
+        {
+            top = null;
+            count = 0;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public void Clear() => top = null;
+        
+
+        public int Peek()
+        {
+            return top.Data;
+        }
+
+        public int Pop()
+        {
+            int value = top.Data;
+            top = top.Next;
+            count--;
+            return value;
+        }
+
+        public void Push(int item)
+        {
+            Node<T> newNode = new Node<T>(item);
+            if (top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.Next = top;
+                top = newNode;
+            }
+            count++;
+        }
+    }
+}
