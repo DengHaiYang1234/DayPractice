4dee66f8d7295a3fd5c586bd56705c5838552824
DengHaiYang1234 <429078038@qq.com>
2018-12-06 09:43:28 +0800

    2018.12.6
    链队列及循环队列

---
+++ Day_DataStructure/097.链队列/LinkQueue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class LinkQueue<T> : ILinkQueue<T>
+    {
+        private Node<T> rear; //尾
+        private Node<T> front; //头
+        private int count;
+
+        public LinkQueue()
+        {
+            rear = null;
+            front = null;
+            count = 0;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public void Clear()
+        {
+            rear = front = null;
+            count = 0;
+        }
+
+        public T Dequeue()
+        {
+            T value = front.Data;
+            front = front.Next;
+            count--;
+            return value;
+        }
+
+        public void Enqueue(T item)
+        {
+            Node<T> newItem = new Node<T>(item);
+            if (rear == null)
+            {
+                front = newItem;
+                rear = newItem;
+            }
+            else
+            {
+                rear.Next = newItem;
+                rear = newItem;
+            }
+            count++;
+        }
+
+        public bool IsEmpty() => count == 0;
+        
+
+        public T Peek()
+        {
+            return front.Data;
+        }
+    }
+}
---
+++ Day_DataStructure/098.循环队列/LoopQueue.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class LoopQueue<T> : ILoopQueue<T>
+    {
+        private T[] arr;
+        private int front;
+        private int rear;
+        private int MAXSIZE;
+
+        public LoopQueue(int len)
+        {
+            arr = new T[len];
+            front = rear = 0;
+            MAXSIZE = len;
+        }
+
+        public LoopQueue() : this(5)
+        {
+            
+        }
+
+        public int Count
+        {
+            get { return (rear - front + MAXSIZE)%MAXSIZE; }
+        }
+
+        public void Clear()
+        {
+            front = rear = 0;
+        }
+
+        public T Dequeue()
+        {
+            T value = arr[front];
+            front = (front + 1) & MAXSIZE;
+            return value;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (((rear + 1)%MAXSIZE) == front)
+            {
+                Console.WriteLine("Is Full");
+                return;
+            }
+
+            arr[rear] = item;
+            rear = (rear + 1)%MAXSIZE;
+        }
+
+        public bool IsEmpty()
+        {
+            return (rear == 0) && (front == 0);
+        }
+
+        public T Peek()
+        {
+            return arr[front];
+        }
+    }
+}
