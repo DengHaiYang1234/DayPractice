c6ef6bf00df91fba181f90cb2103fec470ad4d05
DengHaiYang1234 <429078038@qq.com>
2018-12-07 09:47:31 +0800

    2018.12.7
    数据结构破败啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦！！！！！！！！！

---
+++ Day_DataStructure/099.双栈实现队列/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static Stack<int> stack_1 = new Stack<int>();
+        public static Stack<int> stack_2 = new Stack<int>();
+
+        static void Main(string[] args)
+        {
+            Enqueue(1);
+            Enqueue(2);
+            Enqueue(3);
+            Enqueue(4);
+            Enqueue(5);
+            Console.WriteLine("=======================");
+            Dequeue();
+            Dequeue();
+        }
+
+        static void Enqueue(int value)
+        {
+            Console.WriteLine("Enqueue:" + value);
+            stack_1.Push(value);
+        }
+
+        static int Dequeue()
+        {
+            while (stack_1.Count > 0)
+            {
+                stack_2.Push(stack_1.Pop());
+            }
+
+            int value = stack_2.Pop();
+            Console.WriteLine("Dequeu:" + value);
+            return value;
+        }
+    }
+}
---
+++ Day_DataStructure/100.双队列实现栈/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static Queue<int> queue_1 = new Queue<int>();
+        public static Queue<int> queue_2 = new Queue<int>();
+
+        static void Main(string[] args)
+        {
+            Push(1);
+            Push(2);
+            Push(3);
+            Push(4);
+            Push(5);
+            Console.WriteLine("=====================");
+            Pop();
+            Pop();
+        }
+
+        static void Push(int value)
+        {
+            Console.WriteLine("Push:" + value);
+            queue_1.Enqueue(value);
+        }
+
+        static int Pop()
+        {
+            while (queue_1.Count > 1)
+                queue_2.Enqueue(queue_1.Dequeue());
+
+            int value = queue_1.Dequeue();
+            Console.WriteLine("Pop:" + value);
+
+            while (queue_2.Count > 0)
+                queue_1.Enqueue(queue_2.Dequeue());
+
+            return value;
+        }
+    }
+}
