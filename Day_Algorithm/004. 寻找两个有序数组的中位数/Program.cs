d874cb1a5993b007457c36fcc7093b5711f81d6d
DengHaiYang1234 <429078038@qq.com>
2018-12-24 10:50:42 +0800

    2018.12.24
    算法+双向链表

---
+++ Day_Algorithm/004. 寻找两个有序数组的中位数/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[] { 1, 2 ,5};
+            int[] nums2 = new int[] { 3, 4 };
+            double value = FindMedianSortedArrays(nums1, nums2);
+            Console.WriteLine("中位数：" + value);
+        }
+
+        static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] newArr = Merge(nums1, nums2);
+            double midValue = 0.0;
+  
+            Sort(newArr, 0, newArr.Length - 1);
+
+            if (newArr.Length%2 == 0)
+            {
+                int midIndex_1 = newArr.Length/2;
+                int midIndex_2 = midIndex_1 - 1;
+                midValue = (double)(newArr[midIndex_1] + newArr[midIndex_2])/2;
+            }
+            else if(newArr.Length %2 == 1)
+            {
+                double midIndex = Math.Ceiling((double)(newArr.Length/2));
+                midValue = newArr[(int)midIndex];
+            }
+
+            return midValue;
+        }
+
+        static int[] Merge(int[] nums1, int[] nums2)
+        {
+            int len = nums1.Length + nums2.Length;
+            int[] newArr = new int[len];
+            int index = 0;
+
+            while (index < len)
+            {
+                if (index < nums1.Length)
+                {
+                    newArr[index] = nums1[index];
+                }
+                else if (index < len)
+                {
+                    newArr[index] = nums2[nums2.Length - (len - index)];
+                }
+                index++;
+            }
+
+            return newArr;
+        }
+
+
+        static void Sort(int[] arr,int startIndex,int endIndex)
+        {
+            if (startIndex >= endIndex)
+            {
+                return;
+            }
+
+            int midIndex = SortAndGetMidIndex(arr, startIndex, endIndex);
+            Sort(arr, startIndex, midIndex - 1);
+            Sort(arr, midIndex + 1, endIndex);
+        }
+
+
+        static int SortAndGetMidIndex(int[] arr,int startIndex,int endIndex)
+        {
+            int baseValue = arr[startIndex];
+            while (startIndex < endIndex)
+            {
+                while (startIndex < endIndex && baseValue < arr[endIndex])
+                    endIndex --;
+
+                arr[startIndex] = arr[endIndex];
+
+                while (startIndex < endIndex && baseValue >= arr[startIndex])
+                    startIndex++;
+
+                arr[endIndex] = arr[startIndex];
+            }
+
+            arr[startIndex] = baseValue;
+            return startIndex;
+        }
+    }
+}
---
+++ Day_DataStructure/107.双链表/DoubleLinkList.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class DoubleLinkList
+    {
+        private Node head;
+        private int count;
+
+        public DoubleLinkList()
+        {
+            head = null;
+            count = 0;
+        }
+
+
+        public Node GetElem(int index)
+        {
+            if (index < 0 || index > count)
+            {
+                Console.WriteLine("Not Find");
+                return null;
+            }
+
+
+
+            if (index < count/2)
+            {
+                Node temp = head;
+                for (int i = 0; i < index; i++)
+                {
+                    temp = temp.Next;
+                }
+                return temp;
+            }
+
+            Node temp_ = head;
+            int preIndex = count - index;
+            for (int i = 0; i < preIndex; i++)
+            {
+                temp_ = temp_.Pre;
+            }
+
+            return temp_;
+        }
+
+        public int GetFirst() => GetElem(0).Data;
+        public int GetLast() => GetElem(count - 1).Data;
+        public int Get(int index) => GetElem(index).Data;
+
+
+        public void Append(int index,int value)
+        {
+            if (index == 0 && head == null)
+            {
+                Node newNode = new Node(value,null,null);
+                head = newNode;
+                head.Pre = head;
+                head.Next = head;
+            }
+            else
+            {
+                Node iNode = GetElem(index);
+                Node _newNode = new Node(value, iNode, iNode.Next);
+                iNode.Next.Pre = _newNode;
+                iNode.Next = _newNode;
+            }
+
+            count++;
+        }
+
+        public void Insert(int index,int value)
+        {
+            if (index == 0 && head == null)
+            {
+                Node _newNode = new Node(value, null, null);
+                head = _newNode;
+                head.Pre = head;
+                head.Next = head;
+            }
+            else
+            {
+                Node iNode = GetElem(index);
+                Node newNode = new Node(value, iNode.Pre, iNode);
+                iNode.Pre.Next = newNode;
+                iNode.Pre = newNode;
+                if (index == 0)
+                {
+                    head = newNode;
+                }
+            }
+            count++;
+        }
+
+        public void Del(int index)
+        {
+            Node iNode = GetElem(index);
+            iNode.Pre.Next = iNode.Next;
+            iNode.Next.Pre = iNode.Pre;
+            if (index == 0)
+            {
+                head = iNode;
+            }
+            count--;
+        }
+
+        public void DelFirst() => Del(0);
+        public void DelLast() => Del(count - 1);
+
+        public void ShowAll()
+        {
+            Console.WriteLine("---------Start--------");
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("<" + i + ">:" + Get(i));
+            }
+            Console.WriteLine("---------End--------");
+        }
+    }
+}
