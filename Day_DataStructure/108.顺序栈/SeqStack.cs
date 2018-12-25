3de5cf2bfc56cf3a8589dabff499c3d4ff17b7a4
DengHaiYang1234 <429078038@qq.com>
2018-12-25 17:45:35 +0800

    2018.12.25
    顺序栈

---
+++ Day_DataStructure/108.顺序栈/SeqStack.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class SeqStack : ISeqStack
+    {
+        private int[] arr;
+        private int[] min;
+        private int top;
+
+        public SeqStack(int len)
+        {
+            arr = new int[len];
+            min = new int[len];
+            top = -1;
+        }
+
+        public SeqStack() : this(10)
+        {
+            
+        }
+
+        public int Count
+        {
+            get
+            {
+                int count = top;
+                return ++count;
+            }
+        }
+
+        public void Clear() => top = -1;
+        
+        public int Peek()
+        {
+            if (top >= 0)
+            {
+                return arr[top];
+            }
+            return -1;
+        }
+
+        public int Pop()
+        {
+            int value = -1;
+            if (top >= 0)
+            {
+                value = arr[top];
+                top--;
+            }
+
+            return value;
+        }
+
+        public void Push(int item)
+        {
+            arr[top + 1] = item;
+            SetMin(item);
+            top++;
+        }
+
+        public void SetMin(int item)
+        {
+            if (top == -1)
+            {
+                min[top + 1] = item;
+            }
+            else
+            {
+                int topValue = min[top];
+                if (item < topValue)
+                {
+                    min[top + 1] = item;
+                }
+                else
+                {
+                    min[top + 1] = topValue;
+                }
+            }
+        }
+
+        public int GetMin()
+        {
+            if (top > -1)
+            {
+                return min[top];
+            }
+            return -1;
+        }
+    }
+}
