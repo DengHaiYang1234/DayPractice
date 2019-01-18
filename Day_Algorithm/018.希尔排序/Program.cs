c4f2d8967d171351b469df4a105e8f043c583322
DengHaiYang1234 <429078038@qq.com>
2019-01-18 11:53:05 +0800

    2019.1.18
    希尔排序
    顺序循环队列

---
+++ Day_Algorithm/018.希尔排序/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+希尔排序是把记录按下标的一定增量分组，对每组使用直接插入排序算法排序；随着增量逐渐减少，每组包含的关键词越来越多，当增量减至1时，整个文件恰被分成一组，算法便终止。
+参考：https://www.cnblogs.com/chengxiao/p/6104371.html
+*/
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 4, 2, 7, 9, 8, 3, 6 };
+            Sort(arr);
+            for(int i = 0; i < arr.Length;i++)
+                Console.WriteLine(arr[i]);
+        }
+
+        static void Sort(int[] arr)
+        {
+            for (int gap = arr.Length/2; gap > 0; gap /= 2)
+            {
+                for (int i = gap; i < arr.Length; i++)
+                {
+                    int j = i;
+                    int temp = arr[j];
+                    if (arr[j] < arr[j - gap])
+                    {
+                        while (j - gap >= 0 && temp < arr[j - gap])
+                        {
+                            arr[j] = arr[j - gap];
+                            j -= gap;
+                        }
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Day_DataStructure/127.顺序循环队列/SeqLoopQueue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class SeqLoopQueue<T> : ISeqLoopQueue<T>
+    {
+        private T[] arr;
+        private int rear; //尾
+        private int front; //头
+        private int MAXSIZE; 
+
+        public SeqLoopQueue(int len)
+        {
+            arr = new T[len];
+            rear = 0;
+            front = 0;
+            MAXSIZE = len;
+        }
+        
+        public int Count
+        {
+            get { return (rear - front + MAXSIZE)%MAXSIZE; }
+        }
+
+        public void Clear()
+        {
+            rear = 0;
+            front = 0;
+        }
+
+        public T Dequeue()
+        {
+            T value = arr[front++];
+            front = front%MAXSIZE;
+            return value;
+        }
+
+        public void Enqueue(T item)
+        {
+            if ((rear + 1)%MAXSIZE == 0)
+            {
+                Console.WriteLine("Full");
+                return;
+            }
+            arr[rear++] = item;
+            rear = rear%MAXSIZE;
+        }
+
+        public bool IsEmpty()
+        {
+            return (front == 0) && (rear == 0);
+        }
+
+        public T Peek()
+        {
+            return arr[front];
+        }
+    }
+}
