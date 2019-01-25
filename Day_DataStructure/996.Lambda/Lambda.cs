c1c1c52a408108c7960a421cde7e908dd6225fff
DengHaiYang1234 <429078038@qq.com>
2019-01-25 10:48:44 +0800

    2019.1.25
    Lambda表达式

---
+++ Day_DataStructure/996.Lambda/Lambda.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> func = new Func<int, int, int>((a,b) =>
+            {
+                return a + b;
+            });
+
+            int result = func(100, 200);
+            Console.WriteLine(result);
+
+            func = (x,y) =>
+            {
+                return x * y;
+            };
+
+            result = func(3, 10);
+
+            Console.WriteLine(result);
+        }
+
+
+
+    }
+
+
+}
---
+++ Day_DataStructure/996.Lambda/Lambda方法.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ddd = Test<int>((a, b) =>
+            {
+                return a*b;
+            }, 100,200);
+
+            Console.WriteLine(ddd);
+        }
+
+        static string Test<T>(Func<T, T, T> func, T x, T y)
+        {
+            T result = func(x, y);
+            Console.WriteLine(result);
+
+            return "123";
+        }
+    }
+}
