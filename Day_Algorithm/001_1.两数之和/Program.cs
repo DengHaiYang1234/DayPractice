b9b1f6265799771c10824d7df85377f1da6ca6fa
DengHaiYang1234 <429078038@qq.com>
2019-01-28 10:34:50 +0800

    2018.1.28
    两数之和及无重复字串 算法

---
+++ Day_Algorithm/001_1.两数之和/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+*/
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {2,7,11,15,5,6,9,8,7,1,2,3,5,4,6};
+            int[] arr = GetNums(nums, 19);
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr[1]);
+        }
+
+        static int[] GetNums(int[] nums,int target)
+        {
+            int i = 0;
+            int j = i + 1;
+            int[] indexs = new int[2];
+
+            while (i < nums.Length && j < nums.Length)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    indexs[0] = i;
+                    indexs[1] = j;
+                    break;
+                }
+                else
+                {
+                    if (++j >= nums.Length)
+                    {
+                        ++i;
+                        j = i + 1;
+                    }
+                }
+            }
+
+            return indexs;
+        }
+    }
+}
---
+++ Day_Algorithm/004_1.无重复字符的最长字串/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+
+输入: "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+*/
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "pwwkew";
+            Console.WriteLine(GetNoRepString(str));
+        }
+
+        static string GetNoRepString(string str)
+        {
+            int i = 0;
+            int j = 0;
+            int max = 1;
+            int startIndex = 0;
+
+            while (i < str.Length && j <str.Length - 1)
+            {
+                j++;
+
+                int index = i;
+                bool isHave = false;
+
+                while (index < j && j < str.Length)
+                {
+                    if (str[index++] == str[j])
+                    {
+                        isHave = true;
+                        break;
+                    }
+                }
+
+                if (!isHave)
+                {
+                    if ((j - i +1) > max)
+                    {
+                        max = j - i + 1;
+                        startIndex = i;
+                    }
+                }
+                else
+                {
+                    i = j;
+                }
+            }
+            return str.Substring(startIndex, max);
+        }
+    }
+}
