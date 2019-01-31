be45c59f17723d303e20cb67e9851e9a85a44f9e
DengHaiYang1234 <429078038@qq.com>
2019-01-31 21:05:09 +0800

    2019.1.31
    lambda用法

---
+++ Day_DataStructure/996.Lambda/Lambad用法.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Lambad用法
+    {
+        static void Main()
+        {
+
+            Func<int, int, int> fun = (x, y) =>
+            {
+                return x * y;
+            };
+
+            Func<IPowerSupply, int> func = ps => ps.GetPower();
+
+            Console.WriteLine(func(new PowerSupply()));
+
+            Action<int, int> ac = (x, y) =>
+            {
+                Console.WriteLine(x + y);
+            };
+
+            ac(10, 10);
+        }
+
+
+    }
+
+    public interface IPowerSupply
+    {
+        int GetPower();
+    }
+
+    public class PowerSupply : IPowerSupply
+    {
+        public int GetPower()
+        {
+            return 201;
+        }
+    }
+}
