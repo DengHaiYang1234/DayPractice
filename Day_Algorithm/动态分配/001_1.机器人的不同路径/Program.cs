0b5d096b83fc16e161a69c3400743a7c222199c2
DengHaiYang1234 <429078038@qq.com>
2019-02-01 17:50:44 +0800

    2019.2.1
    动态分配
    过年啦 啊哈哈

---
+++ Day_Algorithm/动态分配/001_1.机器人的不同路径/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//001.机器人不同路径
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int Getpath(int m,int n)
+        {
+            int[,] dp = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        dp[i, j] = 1;
+                    }
+                    else
+                    {
+                        dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+                    }
+                }
+            }
+
+            return dp[m - 1, n - 1];
+        }
+    }
+
+
+    
+
+}
---
+++ Day_Algorithm/动态分配/003_1.找零钱/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//找零钱
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] coins = { 1, 2, 5 };
+            Console.WriteLine(GetChange(coins,5));
+        }
+
+        static int GetChange(int[] coins,int target)
+        {
+            int[,] dp = new int[coins.Length,target+1];
+
+            for (int i = 0; i < coins.Length; i++)
+            {
+                dp[i, 0] = 1;
+            }
+
+            for (int i = 0; i <= target; i++)
+            {
+                if (i%coins[0] == 0)
+                {
+                    dp[0, i] = 1;
+                }
+                else
+                    dp[0, i] = 0;
+            }
+
+
+            for (int i = 1; i < coins.Length; i++)
+            {
+                for (int j = 1; j <= target; j++)
+                {
+                    int temp = 0;
+
+                    for (int k = 0; k*coins[i] <= j; k++)
+                    {
+                        temp += dp[i - 1, j - k*coins[i]];
+                    }
+
+                    dp[i, j] = temp;
+                }
+            }
+
+            return dp[coins.Length - 1, target];
+        }
+    }
+
+
+
+
+}
