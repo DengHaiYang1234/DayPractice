aa45aa34f993360d07a7e1d03eb1bc75c1b6c63b
DengHaiYang1234 <429078038@qq.com>
2019-02-19 16:13:41 +0800

    2019.2.19
    练习练习.

---
+++ Day_DataStructure/139.线性表/SeqList.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class SeqList<T> : ISeqList<T>
+    {
+        private T[] arr;
+        private int count;
+
+        public SeqList(int len)
+        {
+            arr = new T[len];
+            count = 0;
+        }
+
+        public SeqList() : this(3)
+        {
+            
+        }
+
+        public T this[int index]
+        {
+            get { return arr[index]; }
+        }
+
+        public int Length
+        {
+            get { return count; }
+        }
+
+        public void Add(T item)
+        {
+            if (count >= arr.Length)
+            {
+                T[] newArr = new T[count*2];
+                for (int i = 0; i < count; i++)
+                {
+                    newArr[i] = arr[i];
+                }
+                newArr[count++] = item;
+                arr = newArr;
+            }
+            else
+            {
+                arr[count++] = item;
+            }
+        }
+
+        public void Clear()
+        {
+            count = 0;
+        }
+
+        public int GetLength() => count;
+        
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index > count)
+            {
+                Console.WriteLine("越界!");
+                return;
+            }
+
+            if (count >= arr.Length)
+            {
+                T[] newArr = new T[count * 2];
+                for (int i = 0; i < count; i++)
+                {
+                    newArr[i] = arr[i];
+                }
+                arr = newArr;
+            }
+
+            for (int i = arr.Length - 1; i > index; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+
+            arr[index] = item;
+            count++;
+        }
+
+        public bool IsExist(T item) => IndexOf(item) == -1;
+        
+        public T Remove(T item)
+        {
+            int index = IndexOf(item);
+            
+            if (index == -1)
+            {
+                Console.WriteLine("未找到");
+                return default(T);
+            }
+
+            for (int i = index; i < count - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+
+            count--;
+            return item;
+        }
+
+        public T RemoveAt(int index)
+        {
+            if (index == -1)
+            {
+                Console.WriteLine("未找到");
+                return default(T);
+            }
+
+            T value = arr[index];
+
+            for (int i = index; i < count - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            count--;
+            return value;
+        }
+    }
+}
---
+++ Day_DataStructure/141.双向循环链表/DoubleLinkList.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class DoubleLinkList
+    {
+        private Node head;
+        private int count;
+
+        public DoubleLinkList()
+        {
+            head = null;
+            count = 0;
+        }
+
+        public Node GetElem(int index)
+        {
+            if (index < 0 || index > count)
+            {
+                Console.WriteLine("元素未找到");
+                return null;
+            }
+
+            int mid = count/2;
+
+            if (index < mid)
+            {
+                Node temp = head;
+
+                for (int i = 0; i < index; i++)
+                    temp = temp.Next;
+
+                return temp;
+            }
+            else
+            {
+                Node _temp = head;
+                index = count - index;
+                for (int i = 0; i < index; i++)
+                    _temp = _temp.Pre;
+
+                return _temp;
+            }
+        }
+
+        public int GetFirst() => GetElem(0).Data;
+        public int GetLast() => GetElem(count - 1).Data;
+        public int Get(int index) => GetElem(index).Data;
+
+        public void Append(int index,int value)
+        {
+            if (head == null)
+            {
+                head = new Node(value, null, null);
+                head.Pre = head;
+                head.Next = head;
+            }
+            else
+            {
+                Node iNode = GetElem(index);
+                Node newNode = new Node(value,iNode,iNode.Next);
+                iNode.Next.Pre = newNode;
+                iNode.Next = newNode;
+            }
+            count++;
+        }
+
+        public void Insert(int index,int value)
+        {
+            if (head == null)
+            {
+                head = new Node(value, null, null);
+                head.Pre = head;
+                head.Next = head;
+            }
+            else
+            {
+                Node iNode = GetElem(index);
+                Node newNode = new Node(value, iNode.Pre, iNode);
+                iNode.Pre.Next = newNode;
+                iNode.Pre = newNode;
+                if (index == 0)
+                {
+                    head = newNode;
+                }
+            }
+            count++;
+        }
+
+        public void Delete(int index)
+        {
+            Node iNode = GetElem(index);
+            iNode.Pre.Next = iNode.Next;
+            iNode.Next.Pre = iNode.Pre;
+
+            if (index == 0)
+            {
+                head = head.Next;
+            }
+            count--;
+        }
+
+        public void DelFirst() => Delete(0);
+        public void DelLast() => Delete(count - 1);
+
+        public void ShowAll()
+        {
+            Console.WriteLine("---------Start--------");
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("<" + i + ">:" + Get(i));
+            }
+            Console.WriteLine("---------End--------");
+        }
+    }
+}
