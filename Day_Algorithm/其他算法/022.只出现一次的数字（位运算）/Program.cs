3ee37df4bb12f476e13fe719c5a450825e9ce0ea
DengHaiYang1234 <429078038@qq.com>
2019-03-06 16:58:37 +0800

    2019.3.6
    一些面试算法练习

---
+++ Day_Algorithm/其他算法/022.只出现一次的数字（位运算）/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+只出现一次的数字
+*/
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5,5,1,1,6,7,6,8,8};
+
+            Console.WriteLine(arr[Solution(arr)]);
+
+            Console.WriteLine(NewSolution(arr));
+        }
+
+        static int Solution(int[] arr)
+        {
+            int i = 0;
+            int j = i + 1;
+            int index = -1;
+
+            while (i < arr.Length)
+            {
+                if (j >= arr.Length)
+                {
+                    index = i;
+                    break;
+                }
+
+                if (arr[i] == arr[j])
+                {
+                    if (j == i + 1)
+                    {
+                        i = i + 2;
+                    }
+                    else
+                        i++;
+
+                    j = i + 1;
+                }
+                else
+                {
+                    if (j < arr.Length)
+                    {
+                        j++;
+                    }
+                }
+            }
+
+            if (index == -1)
+            {
+                Console.WriteLine("未找到");
+                return -1;
+            }
+
+            return index;
+        }
+
+
+        static int NewSolution(int[] arr)
+        {
+            int num = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                num = num ^ arr[i];
+            }
+
+            return num;
+        }
+    }
+}
---
+++ Day_Algorithm/字符串/001.翻转字符/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+反转字符串
+*/
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] str = { 'H', 'a', 'n', 'n', 'a' ,'h'};
+            char[] arr = Solution(str);
+            char[] arr1 = NewSolution(str);
+
+            Console.WriteLine(new string(arr));
+            Console.WriteLine(new string(arr1));
+            
+            
+        }
+
+        static char[] Solution(char[] arr)
+        {
+            char[] newChar = new char[arr.Length];
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                newChar[arr.Length - 1 - i] = arr[i];
+            }
+
+
+            return newChar;
+
+
+        }
+
+        static char[] NewSolution(char[] arr)
+        {
+            char[] newChar = new char[arr.Length];
+
+            int i = 0;
+            int j = arr.Length - 1;
+
+            while (i < arr.Length || j >= 0)
+            {
+                newChar[i++] = arr[j--];
+            }
+
+            return newChar;
+        }
+
+
+        
+    }
+}
