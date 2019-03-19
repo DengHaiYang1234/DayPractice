255d03c680060e964e76421cae432fa1d4ab949e
DengHaiYang1234 <429078038@qq.com>
2019-03-19 17:56:40 +0800

    2019.3.19
    动态分配

---
+++ Day_Algorithm/动态分配/001_3.机器人的不同路径/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//机器人路径
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Path(5,5));
+        }
+
+        static int Path(int m,int n)
+        {
+            int[,] dp = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        dp[i, j] = 1;
+                    }
+                    else
+                    {
+                        dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+                    }
+                }
+            }
+
+            return dp[m- 1, n - 1];
+        }
+    }
+}
