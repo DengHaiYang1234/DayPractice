4a3855354c0803a62f3ddb2253572199d177f6a0
DengHaiYang1234 <429078038@qq.com>
2019-03-21 18:19:48 +0800

    2019.3.21
    动画

---
+++ Other/Ani/AnimationProject/Assets/Scripts/Biker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Biker : MonoBehaviour
+{
+
+    private Animator _ani;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _ani = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    int value =  (int)Input.GetAxisRaw("Vertical");
+        _ani.SetInteger("Vertical", value);
+	    //transform.Translate(transform.forward * value * Time.deltaTime * 4f);
+	}
+}
