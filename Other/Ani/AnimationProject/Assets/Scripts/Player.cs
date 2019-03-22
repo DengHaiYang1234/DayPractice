a37e1cf22dfc6e6b74a91f3ae7a4c171e43170db
DengHaiYang1234 <429078038@qq.com>
2019-03-22 18:20:17 +0800

    2019.3.22
    动画

---
+++ Other/Ani/AnimationProject/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator _ani;
+
+    private int speedID = Animator.StringToHash("Speed");
+    private int isSpeedUpID = Animator.StringToHash("IsSpeedUp");
+    private int horizontalID = Animator.StringToHash("Horizontal");
+
+    private void Start()
+    {
+        _ani = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        _ani.SetFloat(speedID, Input.GetAxis("Vertical"));
+        _ani.SetFloat(horizontalID, Input.GetAxis("Horizontal"));
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _ani.SetBool(isSpeedUpID, true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _ani.SetBool(isSpeedUpID, false);
+        }
+    }
+
+}
